96a633e9855b223373c9a9ca0dac16973b2b82d4
Partha Susarla <mail@spartha.org>
2019-02-20 13:09:52 +1100

    Update entrypoint.sh to ensure we have correct host file

--- Debian/entrypoint.sh
+++ Debian/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Ensure we have core files
 ulimit -c unlimited
+
+# Remove ipv6 entries from /etc/hosts
+cat /etc/hosts | sed '/^::1/ d' > /tmp/hosts
+echo "y" |  cp -f /tmp/hosts /etc/hosts
+
+# TODO: Fix return codes when failing
 ./testscript.sh
 
