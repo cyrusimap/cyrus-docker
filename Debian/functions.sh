02b0b1b71bd48f94dbb4ce867e3730be12aa0314
Sébastien Delafond <sdelafond@gmail.com>
2019-11-25 13:43:17 +0100

    Apply 76bfc82ff0c2d26cbccff9970253bbb5d8266c82 to Debian/functions.sh
    FTR:
    
      commit 76bfc82ff0c2d26cbccff9970253bbb5d8266c82
      Author: ellie timoney <ellie@fastmail.com>
      Date:   Wed Sep 25 11:09:08 2019 +1000
    
          functions.sh: don't ignore cassandane 'make' failures
    
          (why was this ever a thing??)

--- Debian/functions.sh
+++ Debian/functions.sh
@@ -98,8 +98,9 @@ function _cassandane {
     retval=$(_shell make)
 
     if [ ${retval} -ne 0 ]; then
+        # XXX do we need to 'popd >&3' in here???
         echo "WARNING: Could not run Cassandane"
-        return 0
+        return ${retval}
     fi
 
     cp -af cassandane.ini.dockertests cassandane.ini
