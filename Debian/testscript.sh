f3c60418d3ecdcaaca782641aa7e17f6674ce2b7
Partha Susarla <mail@spartha.org>
2020-05-05 08:45:31 +1000

    primitive error handling

--- Debian/testscript.sh
+++ Debian/testscript.sh
@@ -2,16 +2,29 @@
 
 source functions.sh
 
+echo "Updating cyrus-imapd..."
 _cyrusclone
+
+echo "Updating cassandane..."
 _cassandaneclone
+
+echo "Building cyrus-imapd..."
 _cyrusbuild
 retval=$?
 if [ ${retval} -ne 0 ]; then
     exit ${retval}
 fi
+
+echo "Updating JMAPTestSuite..."
 _updatejmaptestsuite
+
+echo "Running Cassandane Tests..."
 _cassandane
 retval=$?
+if [ ${retval} -ne 0 ]; then
+    exit ${retval}
+fi
+
+echo "Generating Test Report..."
 _report
 exit ${retval}
-
