dcbf3ed2646dd60d9c146a4cc7c94b689e4433de
Ricardo Signes <rjbs@semiotic.systems>
2025-01-10 10:39:15 -0500

    cyd makedocs: fail on warnings
    We have a good thing going.  No warnings in our site generation!
    
    This makes all new warnings fatal, so we catch them in CI.  Will this
    be a good idea to have on all the time from now on?  I do not know.
    
    We will find out together.

--- Debian/bin/lib/Cyrus/Docker/Command/makedocs.pm
+++ Debian/bin/lib/Cyrus/Docker/Command/makedocs.pm
@@ -11,7 +11,12 @@ sub execute ($self, $opt, $args) {
   my $root = $self->app->repo_root->child('docsrc');
   chdir $root or die "can't chdir to $root: $!";
 
-  system('make', 'html');
+  # I would prefer to use long form options, but they are not added until
+  # Sphinx v7, and we are using v5 right now. -- rjbs, 2025-01-10
+  #
+  # -n is "--nitpicky"
+  # -W is "--fail-on-warning"
+  system('make', q{SPHINXOPTS=-n -W}, 'html');
   Process::Status->assert_ok('making "html" target');
 }
 
