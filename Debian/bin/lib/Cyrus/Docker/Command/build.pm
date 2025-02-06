fac7d0a0ab13b26f8140fb77602a3fa41537acb2
Matthew Horsfall <wolfsage@gmail.com>
2025-02-06 14:38:00 -0500

    cyd build: Allow --asan and --ubsan
    This instructs build-with-cyruslibs.sh to use -fsanitize=... and also
    configures a suppression file for LeakSanitizer for known hard to fix cunit
    tests.
    
    After the build, you can `cyd test -- --asan` or `cyd test -- --ubsan` and any
    errors will be reported just like with --valgrind

--- Debian/bin/lib/Cyrus/Docker/Command/build.pm
+++ Debian/bin/lib/Cyrus/Docker/Command/build.pm
@@ -7,6 +7,15 @@ use Process::Status;
 
 sub abstract { 'configure, build, and install cyrus-imapd' }
 
+sub opt_spec {
+  return (
+    [ 'sanitizer' => hidden => { one_of => [
+      [ 'asan'  => 'build with AddressSanitizer' ],
+      [ 'ubsan' => 'build with UBSan' ],
+    ] } ],
+  );
+}
+
 sub execute ($self, $opt, $args) {
   my $root = $self->app->repo_root;
   chdir $root or die "can't chdir to $root: $!";
@@ -23,6 +32,29 @@ sub execute ($self, $opt, $args) {
   say "building cyrusversion $version";
 
   $ENV{CFLAGS}="-g -W -Wall -Wextra -Werror";
+
+  if ($opt->sanitizer) {
+    if ($opt->sanitizer eq 'asan') {
+      $ENV{CYRUS_SAN_FLAGS} = '-fsanitize=address';
+
+      my $lsan_opts = $ENV{LSAN_OPTIONS} || "";
+      my $dont_suppress;
+
+      if ($lsan_opts) {
+        my %opts = map { split '=', $_ } split(':', $lsan_opts);
+        if ($opts{supressions} && $opts{supressions} ne "cunit/leaksanitizer.suppress") {
+          warn "Warning! LSAN_OPTIONS already defines a suppressions file so ours will not be used. You may see spurious failures...\n";
+          $dont_suppress = 1;
+        }
+      }
+
+      unless ($dont_suppress) {
+        $ENV{LSAN_OPTIONS} = "$lsan_opts:suppressions=leaksanitizer.suppress";
+      }
+    } elsif ($opt->sanitizer eq 'ubsan') {
+      $ENV{CYRUS_SAN_FLAGS} = '-fsanitize=undefined';
+    }
+  }
 
   my @configopts = qw(
     --enable-autocreate
